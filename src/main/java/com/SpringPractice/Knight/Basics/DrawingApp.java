92be2f3bf2c0579a9c4a66633cdaa0e99c454567
unknown <tarunvats1990@gmail.com>
2017-06-06 22:01:57 -0500

    first commit

---
+++ src/main/java/com/SpringPractice/Knight/Basics/DrawingApp.java
@@ -0,0 +1,9 @@
+package com.SpringPractice.Knight.Basics;
+
+public class DrawingApp 
+{
+	public static void main(String []args)
+	{
+		System.out.println("I am groot");
+	}
+}
---
+++ src/main/java/com/SpringPractice/Knight/Basics/Triangle.java
@@ -0,0 +1,9 @@
+package com.SpringPractice.Knight.Basics;
+
+public class Triangle 
+{
+	public void draw()
+	{
+		System.out.println("Triangle Drawn");
+	}
+}
